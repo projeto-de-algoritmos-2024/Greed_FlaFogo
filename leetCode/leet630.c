2b58ad79af38e9d7233db36798d97ca3d334be14
GuiDutra21 <221021984@aluno.unb.br>
2025-01-19 13:11:29 -0300

    termina a questão 630
    Co-authored-by: JulioR2022 <JulioR2022@users.noreply.github.com>

--- leetCode/leet630.c
+++ leetCode/leet630.c
@@ -1,9 +1,8 @@
 # include <stdio.h>
 # include <stdlib.h>
-// # define swap(a,b) {int t; t = a; a = b; b = t;}
+# define swapHeap(a,b) {int t; t = a; a = b; b = t;}
 
-// #define swap(a, b) do { int t = (a); (a) = (b); (b) = t; } while (0)
-// #define swap(a, b) { int *t = a; a = b; b = t; }
+typedef int Item;
 
 void swap(int *a, int *b) 
 {
@@ -15,73 +14,14 @@ void swap(int *a, int *b)
     b[1] = temp2;
 }
 
+// void swapHeap(int *a, int *b)
+// {
+//     int temp = *a;
+//     *a = b;
+//     *b = temp;
+// }
+
 void insertionSort(int **v, int l, int r)
-{
-    int aux1,aux2,j;
-    int i = l + 1;
-    while(i <= r)
-    {
-        aux1 = v[i][1];
-        aux2 = v[i][0];
-        j = i;
-        while(j  > 0 && aux2 < v[j - 1][0])
-        {
-            v[j][1] = v[j - 1][1];
-            v[j][0] = v[j - 1][0];
-            j--;
-        }
-
-        v[j][1] = aux1;
-        v[j][0] = aux2;
-        i++;
-    }
-}
-
-int partition( int **v , int l , int r )
-{
-    int i = l;
-    int j = l ;
-    int pivo = v[r][0];
-
-    while (i < r)
-    {
-        if(v[i][0] < pivo)
-        {   
-            if(i != j)
-                swap(v[i], v[j]);
-            j++;
-        }
-        i++;
-    }
-    
-    swap (v[j] ,v[r]);
-
-    return j;
-}
-
-void quickSort(int **v, int l, int r)
-{
-    if(l < r)
-    {   
-        // coloca a mediana na ultima posicao, mediana de 3
-        int meio = (l + r)/2; 
-
-        if(v[meio][0] > v[r][0]) swap(v[meio], v[r]);
-        if(v[l][0] > v[meio][0]) swap(v[l], v[meio]);
-        if(v[r][0] > v[meio][0]) swap(v[r],v[meio]);
-
-        if(r - l + 1 <= 15 )
-            insertionSort(v, l, r);
-        else
-        {
-            int p = partition(v, l, r);
-            quickSort(v, l, p-1);
-            quickSort(v, p+1, r);
-        }
-    }
-}
-
-void insertionSort1(int **v, int l, int r)
 {
     int aux1,aux2,j;
     int i = l + 1;
@@ -103,7 +43,7 @@ void insertionSort1(int **v, int l, int r)
     }
 }
 
-int partition1( int **v , int l , int r )
+int partition( int **v , int l , int r )
 {
     int i = l;
     int j = l ;
@@ -125,7 +65,7 @@ int partition1( int **v , int l , int r )
     return j;
 }
 
-void quickSort1(int **v, int l, int r)
+void quickSort(int **v, int l, int r)
 {
     if(l < r)
     {   
@@ -137,13 +77,46 @@ void quickSort1(int **v, int l, int r)
         if(v[r][1] > v[meio][1]) swap(v[r],v[meio]);
 
         if(r - l + 1 <= 15 )
-            insertionSort1(v, l, r);
+            insertionSort(v, l, r);
         else
         {
-            int p = partition1(v, l, r);
-            quickSort1(v, l, p-1);
-            quickSort1(v, p+1, r);
+            int p = partition(v, l, r);
+            quickSort(v, l, p-1);
+            quickSort(v, p+1, r);
         }
+    }
+}
+
+void  fixUp(Item *heap, Item n)
+{   
+    // n é o índice no vetor
+    while( n > 1 && heap[n/2] < heap[n])
+    {
+        swapHeap(heap[n/2],heap[n]);
+        n = n/2;
+    }
+}
+
+// k = índice do elemento em análise
+void fixDown(Item k, Item n, Item *heap)
+{   
+    int j;
+
+    // Tem filho?
+    while(2*k <= n)
+    {
+        j = 2 * k; // recebe o índice do filho
+
+        // Pega o maior filho
+        if(j < n && heap[j] < heap[j + 1])
+            j++;
+        
+        // Se o maior filho for menor que o pai ja pode parar
+        if(heap[k] > heap[j])
+            return;
+
+        swapHeap(heap[k], heap[j]);
+        k = j; // Atualiza o índice do elemento
     }
 }
 
@@ -152,68 +125,32 @@ int scheduleCourse(int** courses, int coursesSize, int* coursesColSize)
     int actual_day = 0;
     int res = 0;
     actual_day = 0;
-    quickSort1(courses,0,coursesSize-1);
+    quickSort(courses,0,coursesSize-1);
+    int n = 1;
+    int *heap = malloc(sizeof(int) * (coursesSize + 1) );
 
-    int ver_first = 0;
-    int first = 0 ;
     for(int i = 0; i < coursesSize; i++)
     {
         if(actual_day + courses[i][0] <= courses[i][1])
-        {
-            if(!ver_first)
-            {
-                first = courses[i][0];
-                ver_first = 1;
-            }
-            else if(courses[i][0] < first)
-            {
-                first = courses[i][0];
-            }
-            //printf("%d %d\n",actual_day + courses[i][0], courses[i][1]);
+        {   
+            printf("actual_day: %d dur: %d temp:%d",courses[i][0], courses[i][1]);
+
+            heap[n] = courses[i][0];
+            fixUp(heap, n);
+            n++;
             res++;
             actual_day += courses[i][0];
         }
-        else if(courses[i][0] < first && courses[i][0] <= courses[i][1])
+        else if(courses[i][0] < heap[1])
         {
-            actual_day -= first;
+            actual_day -= heap[1];
             actual_day += courses[i][0];
-            first = courses[i][0];
+            heap[1] = courses[i][0];
+            fixDown(1, n-1, heap);
         }
     }
 
+    printf("%d\n", res);
     return res;
     
 }
-
-int main()
-{
-    int **v = malloc(2 * sizeof(int*));
-    v[0] = malloc(2 * sizeof(int));
-    v[0][0] = 3;
-    v[0][1] = 2;
-    v[1] = malloc(2 * sizeof(int));
-    v[1][0] = 4;
-    v[1][1] = 3;
-
-    int *coursesColSize = malloc(sizeof(int));
-    *coursesColSize = 2;
-    scheduleCourse(v,2,coursesColSize);
-
-    return 0;
-}
-// int scheduleCourse(int** courses, int coursesSize, int* coursesColSize) 
-// {
-//     int actual_day = 0;
-//     int courses_complete = 0;
-//     quickSort(courses, 0, coursesColSize - 1);
-//     int **courses_sorted = courses;
-//     for(int i = 0; i < coursesSize; i++ )
-//     {
-//         // Consegue completar o curso ate o dia limite ?
-//         if(actual_day + courses_sorted[i][0] <= courses_sorted[i][1])
-//         {
-//             courses_complete++;
-//             actual_day += courses_sorted[i][0];
-//         }
-//     }
-// }
